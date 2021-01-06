e81b7e1ef38c74d6113e99c0817e40a8afd01a92
Beksultan Maukenov <bmaukenov@gmail.com>
2021-01-07 03:58:15 +0600

    Update
    Before I have accidentally put there old version. Posted new version plus made some minor changes to be in accordance with PEP8

--- HW Requests.py
+++ HW Requests.py
@@ -1,64 +1,49 @@
-import os
 import requests
-IAM_TOKEN = 't1.9euelZrPkJ2dlcuYlcyejJLHicjOku3rnpWazZDNio6dzMaVz5bKl8-SkY_l8_dbDyoA-u9vICsY_d3z9xs-JwD6728gKxj9.Z8fypFvMaBAQB_TMIC0q0mlzsH4azsCiWC91YdmkAYwsfAnm3HxmALifnd271uLgePzlx8IXlzA6k3qrP_hkDw'
-FOLDER_ID = os.getenv('b1gitleg3ur0o1bmovtm')
-print(IAM_TOKEN)
-def translate(texts):
+
+
+def translate(initial_file_path, final_file_path, to_lang):
+    IAM_TOKEN = 't1.9euelZqLkJ2NzcePlcmOzY2YyMiOme3rnpWazZDNio6dzMaVz5bKl8-SkY_l8_dtRycA-u8fcwh6_d3z9y12JAD67x9zCHr9.qTg4sFumULAbPgb58klXqTWpSDrDqY1FzOWq-tuy8CBnQLLzVh77BavOYskL9p6cnbPsVsesdTfttwpvTgl9Aw'
+    FOLDER_ID = 'b1gitleg3ur0o1bmovtm'
     url = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
+    with open(initial_file_path) as file:
+        texts = file.read()
     headers = {
         'Authorization': f'Bearer {IAM_TOKEN}',
         'Content-Type': 'application/json',
     }
-
-    params = {
-  "sourceLanguageCode": "fr",
-  "targetLanguageCode": "ru",
-  "format": "PLAIN_TEXT",
-  "texts": [
-    texts
-  ],
-  "folderId": FOLDER_ID,
-
-    }
     data = {
         'folder_id': FOLDER_ID,
         'texts': texts,
-        'targetLanguageCode': 'ru'
+        'targetLanguageCode': to_lang
     }
-    response = requests.post(url, headers=headers, params = params, json=data)
+    response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
     data = response.json()
-    return data.get('translations', [])
-
-    with open(result_file_path, "w") as result_file:
-        result_file.write(result)
-
+    with open(final_file_path, "w") as file:
+        file.write(str(data.get('translations', [])[0]["text"]))
+    return final_file_path
 
 
-
-
-def yandex_download():
+def yandex_disk_download(file_path):
+    file_name = file_path.split("/")[-1] + ".txt"
     URL = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-
-    params = {"path" : "6 января.txt", "overwrite" : "true"}
-
-    headers = {"Authorization" : "OAuth AgAAAAAZdNVWAAbNc6C12UBVhEYPoMwFNBzlJXI"}
-
-    req = requests.get(URL, params = params, headers = headers)
-    trial = req.json()["href"]
-    files = {'file': open('/Users/beksultan_maukenov/Desktop/py-homework-basic-files/3.2.http.requests/FRtest.txt', 'rb')}    
-    req_2 = requests.put(trial, files = files)
-
-
+    params = {"path": file_name, "overwrite": "true"}
+    headers = {"Authorization": "OAuth AgAAAAAZdNVWAAbNc6C12UBVhEYPoMwFNBzlJXI"}
+    first_req = requests.get(URL, params=params, headers=headers)
+    second_req = first_req.json()
+    download_link = second_req["href"]
+    file = {"file": open(file_path)}
+    downloading = requests.put(download_link, files=file)
+    downloading.raise_for_status()
+    if str(downloading) == "<Response [201]>":
+        print("Done")
+    else:
+        print("There are some technical problems, sorry")
 
 
 if __name__ == '__main__':
-    initial_file_path = "/Users/beksultan_maukenov/Desktop/py-homework-basic-files/3.2.http.requests/FRtest.txt"
-    result_file_path = "/Users/beksultan_maukenov/Desktop/result.txt"
-
-
-    with open(initial_file_path) as initial_file:
-        texts = initial_file.read()
-        result = translate(texts)
-        yandex_download(result)
-
+    initial_file_path = '/Users/beksultan_maukenov/Desktop/py-homework-basic-files/3.2.http.requests/FRtest.txt'
+    final_file_path = '/Users/beksultan_maukenov/Desktop/py-homework-basic-files/3.2.http.requests/Result_FRtest.txt'
+    to_lang = "ru"
+    file_path = translate(initial_file_path, final_file_path, to_lang)
+    yandex_disk_download(file_path)

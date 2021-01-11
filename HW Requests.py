e27e7a364057dd287c0e4cc49bce1324ed70265a
Beksultan Maukenov <bmaukenov@gmail.com>
2021-01-11 22:06:40 +0600

    Mistakes corrected

--- HW Requests.py
+++ HW Requests.py
@@ -1,15 +1,16 @@
 import requests
+import os
 
 
-def translate(initial_file_path, final_file_path, to_lang):
-    IAM_TOKEN = ''
-    FOLDER_ID = ''
+def translate(initial_file_path, final_file_path, from_lang, to_lang="ru"):
+    IAM_TOKEN = os.getenv("IAM_TOKEN")
+    FOLDER_ID = os.getenv('FOLDER_ID')
     url = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
     with open(initial_file_path) as file:
         texts = file.read()
     headers = {
         'Authorization': f'Bearer {IAM_TOKEN}',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     }
     data = {
         'folder_id': FOLDER_ID,
@@ -17,7 +18,6 @@ def translate(initial_file_path, final_file_path, to_lang):
         'targetLanguageCode': to_lang
     }
     response = requests.post(url, headers=headers, json=data)
-    response.raise_for_status()
     data = response.json()
     with open(final_file_path, "w") as file:
         file.write(str(data.get('translations', [])[0]["text"]))
@@ -28,13 +28,13 @@ def yandex_disk_download(file_path):
     file_name = file_path.split("/")[-1] + ".txt"
     URL = "https://cloud-api.yandex.net/v1/disk/resources/upload"
     params = {"path": file_name, "overwrite": "true"}
-    headers = {"Authorization": "OAuth AgAAAAAZdNVWAAbNc6C12UBVhEYPoMwFNBzlJXI"}
+    Ya_Disk_Token = os.getenv("Ya_Disk_Token")
+    headers = {"Authorization": f"OAuth {Ya_Disk_Token}"}
     first_req = requests.get(URL, params=params, headers=headers)
     second_req = first_req.json()
     download_link = second_req["href"]
     file = {"file": open(file_path)}
     downloading = requests.put(download_link, files=file)
-    downloading.raise_for_status()
     if str(downloading) == "<Response [201]>":
         print("Done")
     else:
@@ -42,8 +42,9 @@ def yandex_disk_download(file_path):
 
 
 if __name__ == '__main__':
-    initial_file_path = '/Users/beksultan_maukenov/Desktop/py-homework-basic-files/3.2.http.requests/FRtest.txt'
-    final_file_path = '/Users/beksultan_maukenov/Desktop/py-homework-basic-files/3.2.http.requests/Result_FRtest.txt'
+    initial_file_path = os.getcwd() + "/text-to-translate.txt"
+    final_file_path = os.getcwd() + "/translated-text.txt"
+    from_lang = "fr"
     to_lang = "ru"
-    file_path = translate(initial_file_path, final_file_path, to_lang)
+    file_path = translate(initial_file_path, final_file_path, from_lang, to_lang)
     yandex_disk_download(file_path)
